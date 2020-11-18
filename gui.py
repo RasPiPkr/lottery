d9cddc7a9494a3c7dd64f97dfb7642a0ecc844ac
RasPiPkr <raspipkr@gmail.com>
2020-11-18 15:53:25 +0000

    Add National Lottery

--- gui.py
+++ gui.py
@@ -18,9 +18,11 @@ def changeMode(bgMode, txtMode):
     menu.config(bg=bgMode, fg=txtMode)
     dispFrame.config(bg=bgMode)
 
-def getNumbers(tickets, main_low, main_high, main_picks, is_star_picks, star_low_range, star_high_range, star_picks):
+def getNumbers(tickets, main_low, main_high, main_picks, has_stars, star_low, star_high, star_picks):
+    print(tickets, main_low, main_high, main_picks, has_stars, star_low, star_high, star_picks)
     for ticket in range(int(tickets)):
-        nums = [i for i in range(main_low, main_high)]
+        nums = [i for i in range(main_low, main_high + 1)]
+        print(nums)
         picked = []
         for i in range(main_picks):
             pick = random.choice(nums)
@@ -28,7 +30,22 @@ def getNumbers(tickets, main_low, main_high, main_picks, is_star_picks, star_low
             picked.append(pick)
             nums.pop(choice)
         picked.sort()
+        if has_stars == True:
+            starNums = [i for i in range(star_low, star_high + 1)]
+            print(starNums)
+            pickedStars = []
+            for i in range(star_picks):
+                pickStar = random.choice(starNums)
+                starChoice = starNums.index(pickStar)
+                pickedStars.append(pickStar)
+                starNums.pop(starChoice)
+            if len(pickedStars) >= 2:
+                pickedStars.sort()
+            picked.append('--')
+            for star in range(len(pickedStars)):
+                picked.append(pickedStars[star])
         showTickets.insert('end', picked)
+        
 
 
 def healthLottery():
@@ -38,6 +55,8 @@ def healthLottery():
     currFG = menu['fg']
     dispFrame = tk.Frame(root, bg=currBG)
     dispFrame.pack(fill='both', expand=True)
+    game = tk.Label(dispFrame, text='Health Lottery', bg=currBG, fg=currFG)
+    game.pack()
     lines = [str(i).zfill(2) for i in range(1, 51)]
     linesLabel = tk.Label(dispFrame, text='How many lines:', bg=currBG, fg=currFG)
     linesLabel.pack(pady=10)
@@ -51,16 +70,40 @@ def healthLottery():
 
 
 def nationalLottery():
-    global dispFrame, showTickets
+    global dispFrame, showTickets, getGame, games, getLines
     dispFrame.destroy()
     currBG = menu['bg']
     currFG = menu['fg']
     dispFrame = tk.Frame(root, bg=currBG)
     dispFrame.pack(fill='both', expand=True)
+    games = ['Lotto', 'Thunderball', 'Euromillions']
+    getGame = ttk.Combobox(dispFrame, values=games)
+    getGame.set('Lotto')
+    getGame.pack()
     lines = [str(i).zfill(2) for i in range(1, 51)]
     linesLabel = tk.Label(dispFrame, text='How many lines:', bg=currBG, fg=currFG)
     linesLabel.pack(pady=10)
+    getLines = ttk.Combobox(dispFrame, width=3, values=lines)
+    getLines.set('01')
+    getLines.pack()
+    if getGame.get() == 'Lotto':
+        getTickets = tk.Button(dispFrame, text='Generate Ticket(s)', bg=currBG, fg=currFG, command=nationalLotteryHandler)
+    elif getGame.get() == 'Thunderball':
+        getTickets = tk.Button(dispFrame, text='Generate Ticket(s)', bg=currBG, fg=currFG, command=nationalLotteryHandler)
+    elif getGame.get() == 'Euromillions':
+        getTickets = tk.Button(dispFrame, text='Generate Ticket(s)', bg=currBG, fg=currFG, command=nationalLotteryHandler)
+    getTickets.pack(pady=10)
+    showTickets = tk.Listbox(dispFrame, width=20, height=10)
+    showTickets.pack()
 
+
+def nationalLotteryHandler():
+    if getGame.get() == games[0]:
+        getNumbers(getLines.get(), 1, 59, 6, False, 0, 0, 0)
+    elif getGame.get() == games[1]:
+        getNumbers(getLines.get(), 1, 39, 5, True, 1, 14, 1)
+    elif getGame.get() == games[2]:
+        getNumbers(getLines.get(), 1, 50, 5, True, 1, 12, 2)
 
 
 # Setup root window
